c10b02d3a61108ed38d297642377eb04700841d9
Callum Eden <callum.eden@essensys.tech>
2017-10-23 15:49:49 +0100

    [CE, BO] : Implementing rand & fifo eviction

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import javafx.util.Pair;
+
 import java.util.*;
 
 public class Main {
 
-    private static int front = -1;
+    private static Queue<Pair<Integer, Integer>> retrievalQueue = new ArrayDeque<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -19,64 +21,119 @@ public class Main {
                 cache = new RANDCache(m);
                 break;
             case "FIFO":
-                cache = new FIFOCache();
+                cache = new FIFOCache(m);
                 break;
             default:
                 System.err.println("Unknown strategy!");
+                return;
         }
+
+        System.out.println(cache);
 
     }
 
     private static abstract class Cache {
         Set<Integer> knownVals = new HashSet<>();
 
+        public Cache(int size) {
+            populateCache(size);
+        }
+
+        private void populateCache(int size) {
+            for (int i = 1; i <= size; i ++) {
+
+                addToStore(i);
+                knownVals.add(i);
+            }
+
+        }
+
         public boolean retrieve(int val) {
             if (knownVals.contains(val)) {
                 return true;
             }
-            evict();
-            addToStore(val);
+            replace(val);
             knownVals.add(val);
             return false;
         }
 
-        abstract void evict();
-        abstract void addToStore(int val);
+        protected abstract void replace(int val);
+        protected abstract void addToStore(int val);
     }
 
     private static class FIFOCache extends Cache {
-        private Queue<Integer> store = new ArrayDeque<>();
+        private Queue<Integer> store;
 
+        public FIFOCache(int size) {
+            super(size);
+        }
 
-        @Override
-        void evict() {
+        private void evict() {
             int removed = store.poll();
             knownVals.remove(removed);
         }
 
         @Override
-        void addToStore(int val) {
+        protected void replace(int val) {
+            evict();
             store.add(val);
         }
+
+        @Override
+        protected void addToStore(int val) {
+            if (store == null) {
+                store = new ArrayDeque<>();
+            }
+            store.add(val);
+        }
+
     }
 
     private static class RANDCache extends Cache {
-        private List<Integer> store = new ArrayList<>();
-        private int size;
+        private List<Integer> store;
+        private Victimiser victimiser;
 
         public RANDCache(int size) {
-            this.size = size;
-        }
-
-
-        @Override
-        void evict() {
+            super(size);
+            this.victimiser = new Victimiser(size);
 
         }
 
+        private int evict() {
+            int victimPosition = victimiser.next();
+            knownVals.remove(store.get(victimPosition));
+            store.remove(victimPosition);
+            return victimPosition;
+        }
+
         @Override
-        void addToStore(int val) {
+        protected void replace(int val) {
+            int evictPos = evict();
+            store.add(evictPos, val);
+        }
+
+        @Override
+        protected void addToStore(int val) {
+            if (store == null) {
+                store = new ArrayList<>();
+            }
             store.add(val);
         }
+
     }
+
+    private static class Victimiser {
+        private int size;
+        private Random rand;
+
+        public Victimiser(int size) {
+            this.size = size;
+            rand = new Random();
+        }
+
+        public int next() {
+            return rand.nextInt(size);
+        }
+    }
+
 }
