ac771c6341d9ab164d056d9b3b137f71f03090f6
Callum Eden <callum.eden@essensys.tech>
2017-10-25 16:40:05 +0100

    [CE, BO] : Plotting stats for miss rate to calculate where start-up bias ends

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ import cache.Cache;
 import cache.FIFOCache;
 import cache.RANDCache;
 import javafx.util.Pair;
+import org.math.plot.Plot2DPanel;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
@@ -45,19 +48,51 @@ public class Main {
                 return;
         }
 
-        int T = 0;
+        double T = 0;
+        double hitCount = 0;
+        double missCount = 0;
         scheduleInitialArrivals(N);
 
-//        while (true) {
-//            Pair<Integer, Integer> arrivalPair = scheduledArrivals.poll();
-//            T = arrivalPair.getKey();
-//            int arrivalIndex = arrivalPair.getValue();
-//            scheduleNext(T, arrivalIndex);
-//            System.out.println("Time of " + T);
-//        }
+        Plot2DPanel plot = new Plot2DPanel();
+        double x[] = new double[20000];
+        double y[] = new double[20000];
 
-//        System.out.println(cache);
+        for (int i = 0; i < 20000; i++) {
 
+            Pair<Double, Integer> arrivalPair = scheduledArrivals.poll();
+            T = arrivalPair.getKey();
+            int arrivalIndex = arrivalPair.getValue();
+            if (cache.retrieve(arrivalIndex)) {
+                hitCount++;
+            } else {
+                missCount++;
+            }
+            scheduleNext(T, arrivalIndex);
+
+            //Writing stats for graph plot
+            x[i] = T;
+            double missRate = missCount / T;
+            y[i] = missRate;
+
+            if (T >= 1200) {
+                //Safe point to discard existing stats due to start-up bias
+                hitCount = 0;
+                missCount = 0;
+            }
+        }
+
+
+        //Plot miss rate to determine time after which start-up bias ends
+        plot.addLinePlot("Miss rate plot", x, y);
+        plot.addLabel("Time", Color.RED, 280.0, 580.0);
+        plot.addLabel("Miss Rate", Color.RED, 10.0, 265.0);
+        JFrame frame = new JFrame("a plot panel");
+        frame.setSize(600, 600);
+        frame.setContentPane(plot);
+        frame.setVisible(true);
+
+        System.out.println("Number of hits were : " + hitCount);
+        System.out.println("Number of misses were : " + missCount);
     }
 
     private static void scheduleInitialArrivals(int N) {
@@ -66,23 +101,16 @@ public class Main {
         }
     }
 
-    private static void scheduleNext(int T, int num) {
+    private static void scheduleNext(double T, int num) {
         double arrivalTime = getNextArrival(T, num);
-        Pair p = new Pair<>(arrivalTime, num);
+        Pair<Double, Integer> p = new Pair<>(arrivalTime, num);
         scheduledArrivals.add(p);
-
-        double arrivalSum = 0;
-        for (int i = 0; i < 500; i ++) {
-            arrivalSum += getNextArrival(0, 9);
-        }
-        System.out.println("AVERAGE TIME IS :" + arrivalSum/500.0);
     }
 
-    private static double getNextArrival(int T, int num) {
+    private static double getNextArrival(double T, int num) {
         double rand = Math.random();
         double interval = -Math.log(1 - rand) * num; //lambda = 1/num
         return T + interval;
-
     }
 
 }
